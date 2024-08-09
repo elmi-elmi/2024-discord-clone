936ca6b9944ea1a14a510474e286623b75c1169f
elmi <shahrokhelmi@gmail.com>
2024-08-09 12:03:22 +0330

    feat/ add dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider, SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/providers/theme-providers";
+import { cn } from "@/lib/utils";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -17,15 +19,23 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={font.className}>
-          {/* <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn> */}
-          {children}
+      <html lang="en" suppressHydrationWarning>
+        <body className={
+          cn(
+            font.className,
+            'bg-white dark:bg-[#313338]'
+          )
+        }>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+            storageKey="discord-theme"
+          >
+            {children}
+          </ThemeProvider>
+
         </body>
       </html>
     </ClerkProvider>
